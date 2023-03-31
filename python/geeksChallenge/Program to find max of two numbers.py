6f6129c5a61cfc2395dd2e66cb997c3b7bc3fe9a
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2023-03-31 12:08:09 +0530

    Final Commit

---
+++ python/geeksChallenge/Program to find max of two numbers.py
@@ -0,0 +1,11 @@
+def max(num1,num2):
+    if num1>num2:
+        print(num1," is the maximum number")
+    else:
+        print(num2," is the maximum number")
+
+print("Enter two numbers")
+a = int(input())
+b = int(input())
+
+max(a,b)
