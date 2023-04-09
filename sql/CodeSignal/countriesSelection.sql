d0918ab0e747a9819f0b405e56609673103e59fa
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2023-04-09 09:28:03 +0530

    Create countriesSelection.sql

---
+++ sql/CodeSignal/countriesSelection.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE solution()
+BEGIN
+	/* Write your SQL here. Terminate each statement with a semicolon. */
+	SELECT name,continent,population
+	FROM countries
+	WHERE continent = 'Africa'
+	ORDER BY name ASC;
+END
