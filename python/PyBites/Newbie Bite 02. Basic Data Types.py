c38c8b3dbd22cc2406752bdae7305062b64a66de
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2023-04-17 15:12:45 +0530

    Delete python/PyBites directory

--- python/PyBites/Newbie Bite 02. Basic Data Types.py
+++
@@ -1,4 +0,0 @@
-# Enter your code below this line
-my_integer=29
-my_float = 400.68
-my_lie = "I love data types!"
--- python/PyBites/Newbie Bite 03. Basic Printing.py
+++
@@ -1,2 +0,0 @@
-# Enter your code below this line:
-print("I find it unbearable that there are people out there who think DC is better than Marvel!")
