ad83a2f093358f8033fe1aa8d7dc5bcd5bcdadab
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2023-04-28 09:20:29 +0530

    Final Commit

---
+++ python/Pynative/Python List Exercise/Exercise 2: Concatenate two lists index-wise.py
@@ -0,0 +1,9 @@
+#Enter Python code here and hit the Run button.
+
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+list3 = []
+x = len(list1)
+for i in range(x):
+    list3.append(list1[i]+list2[i])
+print(list3)
